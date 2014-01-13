a773656d1e922ea3fdeed83614be6b3fbdf41e98
Naga Vijayapuram <nvijayap@gmail.com>
2014-01-12 19:53:20 -0800

    Added controller and view files

---
+++ app/controllers/spellcheck_controller.rb
@@ -0,0 +1,22 @@
+#
+# SpellCheckController
+#
+class SpellcheckController < ApplicationController
+  
+  #
+  # view
+  #
+  def view
+    word = params[:q]
+    word = "" if word.nil?
+    
+    if MyWords.words.has_key? word.downcase
+      params[:yn] = true 
+    else
+      params[:yn] = false  
+    end
+    
+    render
+  end # view
+  
+end # SpellCheckController
